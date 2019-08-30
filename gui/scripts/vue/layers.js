e70961616d6f1e7a80f0b4b7e28e8fe981c70c92
pappkasse <pappkasse2121@gmail.com>
2019-08-31 00:27:27 +0200

    Add deleteLayer method

--- gui/scripts/vue/layers.js
+++ gui/scripts/vue/layers.js
@@ -7,3 +7,15 @@ module.exports.createLayer = function(name = "New Layer", selectLayer = true) {
 
     return newLayer;
 }
+module.exports.deleteLayer = function(layer = this.project.drawing.layers.selected) {
+    if (!layer) return;
+
+    let zPos = layer.z;
+    this.project.drawing.layers.remove(layer);
+
+    if (this.project.drawing.layers.getLayerByZ(zPos)) {
+        this.project.drawing.layers.select(
+            this.project.drawing.layers.getLayerByZ(zPos - 1)
+        );
+    }
+}
